75e702ca469f6f311fbe16f2ff50863d4e94ce4f
Ziv Zamechek <ziv921@yahoo.com>
2017-01-14 14:20:39 -0500

    Added new and create action to the events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,10 +13,25 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    event_params = params.require(:event).
-                    permit(:name, :description, :location, :price, :starts_at)
     @event.update(event_params)
     redirect_to @event
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
+
+  private
+
+    def event_params
+      event_params = params.require(:event).
+      permit(:name, :description, :location, :price, :starts_at)
+    end 
 
 end
