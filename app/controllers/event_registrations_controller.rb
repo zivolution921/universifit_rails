a99c26d584ce72019d44330c0030d5e9c166ac4d
Ziv Zamechek <ziv921@yahoo.com>
2017-01-16 16:08:10 -0500

    Finished event registration views, and clean up

--- app/controllers/event_registrations_controller.rb
+++ app/controllers/event_registrations_controller.rb
@@ -1,6 +1,30 @@
 class EventRegistrationsController < ApplicationController
-  def index
-    @event = Event.find(params[:event_id])
+  before_action :set_event
+  def index   
     @event_registrations = @event.event_registrations
+  end
+
+  def new
+    @event_registration = @event.event_registrations.new
+  end
+
+  def create
+    @event_registration = @event.event_registrations.new(event_registration_params)
+    if @event_registration.save
+      redirect_to event_event_registrations_path(@event),
+                            notice: "Thanks, you're registered!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_registration_params
+    params.require(:event_registration).permit(:name, :email, :how_heard)
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
   end
 end
