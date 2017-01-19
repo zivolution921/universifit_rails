8a2af301cd785c5cfbc75aaf9905afaba1e14f52
Ziv Zamechek <ziv921@yahoo.com>
2017-01-18 19:47:52 -0500

    Add validations to exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,4 +2,9 @@ class Exercise < ApplicationRecord
   belongs_to :profile
 
   validates :workout_date, presence: true
+  validates :duration_in_min, presence: true
+  validates :duration_in_min, numericality: {only_integer: true}
+  validates :workout, presence: true
+  validates :workout_date, presence: true
+  validates :user_id, presence: true
 end
