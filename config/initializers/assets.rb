db5a7436850829a6b222346fba505ed3248109b5
zivolution921 <ziv921@yahoo.com>
2017-03-06 17:47:00 -0500

    added desktop css changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
+
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
@@ -9,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( homepage.css front_end.css front_end.js front_end_bottom.js)
+Rails.application.config.assets.paths << "vendor/assets/fonts/"
