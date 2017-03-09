9c518964a8b67346eb24d26649b2a2af20b28043
zivolution921 <ziv921@yahoo.com>
2017-03-08 22:10:03 -0500

    Adding search action in dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,8 @@ class DashboardController < ApplicationController
   def index
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
+
+  def search 
+    @user = current_user.profile.search_by_name(params[:search_name]).paginate(:page => params[:page])
+  end
 end
