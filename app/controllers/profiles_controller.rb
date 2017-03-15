e47c06b5359b6d697edc9938bc9b8d0387253b70
zivolution921 <ziv921@yahoo.com>
2017-03-15 01:39:49 -0400

    created the activities global variablr that we can see in the show template

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -28,6 +28,7 @@ class ProfilesController < ApplicationController
   def show
     @event_registrations = profile.event_registrations
     @liked_events = profile.liked_events
+    @activities = PublicActivity::Activity.where(owner_id: @user.id)
   end
 
   private
