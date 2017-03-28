3473e3a8246861b95dc32de2e1040cd37b001cc1
zivolution921 <ziv921@yahoo.com>
2017-03-28 09:21:39 -0400

    created friendship

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,4 +4,11 @@ class FriendsController < ApplicationController
     @friends = current_user.friends
   end
 
+  def create
+    @friendship = current_user.request_friendship(@user)
+    respond_to do |format| 
+      format.html {redirect_to user_path, notice: "Friendship Created"}
+    end
+  end
+
 end
