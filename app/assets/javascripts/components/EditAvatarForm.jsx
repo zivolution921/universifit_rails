0fcdabcc167687b301efd0c32d84621b49414d86
zivolution921 <ziv921@yahoo.com>
2017-09-04 13:28:30 -0400

    broken file broken :(

--- app/assets/javascripts/components/EditAvatarForm.jsx
+++ app/assets/javascripts/components/EditAvatarForm.jsx
@@ -3,17 +3,17 @@ class EditAvatarForm extends React.Component {
   render() {
     return (
       <div className="avatar">
-      <p>We do not host images. Please upload your avatar to&nbsp;
-      <a href="https://postimg.org" target="_blank">postimg.org</a> and paste the 'direct' url here.</p>
-      <label htmlFor="avatarUrl">Avatar url:</label><br />
-      <input id='avatarUrl' name='avatarUrl' type="text"
-        value={this.props.avatarUrl}
-        onChange={this.props.handleAvatarChange}/>
-      {this.props.errors 
-        ? <div>{this.props.errors}</div>
-        : null
-      }
-      <button onClick={this.props.handleAvatarSave}>Save</button>
+        <p>We do not host images. Please upload your avatar to&nbsp;
+        <a href="https://postimg.org" target="_blank">postimg.org</a> and paste the 'direct' url here.</p>
+        <label htmlFor="avatarUrl">Avatar url:</label><br />
+        <input id='avatarUrl' name='avatarUrl' type="text"
+          value={this.props.avatarUrl}
+          onChange={this.props.handleAvatarChange}/>
+          {this.props.errors 
+            ? <div>{this.props.errors}</div>
+            : null
+        }
+        <button onClick={this.props.handleAvatarSave}>Save</button>
       </div>
       )
   }
