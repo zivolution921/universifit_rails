79fbb6671da9090f93843738303e8e330f3e868d
alizain <alizain721@gmail.com>
2017-09-25 11:05:53 +0500

    All bugs fixing

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,6 +1,5 @@
 class ExercisesController < ApplicationController
   before_action :authenticate_user!
- 
   def index
     @exercises = current_profile.exercises.persisted
   end
@@ -9,29 +8,37 @@ class ExercisesController < ApplicationController
     exercise
   end
 
-def edit
-end
-
-def update
-  if exercise.update(exercise_params)
-    flash[:success] = "Exercise has been updated"
-    redirect_to profile_exercises_path
-  else
-    flash[:danger] = "Exercise has not been updated"
-    render :edit
+  def edit
   end
-end
+
+  def create
+    @exercise = current_user.profile.exercises.build(exercise_params)
+    if exercise.save
+      flash[:success] = "Exercise has been created"
+      redirect_to [current_user.profile, @exercise]
+    else
+      flash[:danger] = "Exercise has not been created"
+      render partial: "new"
+    end
+  end
+  def update
+    if exercise.update(exercise_params)
+      flash[:success] = "Exercise has been updated"
+      redirect_to profile_exercises_path
+    else
+      flash[:danger] = "Exercise has not been updated"
+      render :edit
+    end
+  end
 
   private
 
 
-  def exercise_params
-    params.require(:exercise).permit(:duration_in_min, :workout, :starts_at, :location, :dedicated_to)
-  end
+    def exercise_params
+      params.require(:exercise).permit(:duration_in_min, :workout,:exercise_category_id, :starts_at, :location, :dedicated_to)
+    end
 
-  def exercise
-    @exercise ||= current_profile.exercises.unscope(:where).find(params[:id])
-  end
-
-
+    def exercise
+      @exercise ||= current_profile.exercises.unscope(:where).find(params[:id])
+    end
 end
