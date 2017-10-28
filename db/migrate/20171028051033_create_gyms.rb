340ff3fbce70156cf847cb7dae7ba9b65bb33a10
alizain <alizain721@gmail.com>
2017-10-28 10:20:25 +0500

    GYM:Fixed migration

--- db/migrate/20171028051033_create_gyms.rb
+++ db/migrate/20171028051033_create_gyms.rb
@@ -1,9 +1,8 @@
 class CreateGyms < ActiveRecord::Migration[5.0]
   def change
     create_table :gyms do |t|
-      t.float, :latitude
+      t.float :latitude
       t.float :longitude
-
       t.timestamps
     end
   end
