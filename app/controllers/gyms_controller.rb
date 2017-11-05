61987dae99092674729e56ecd0329f503b180e8e
alizain <alizain721@gmail.com>
2017-11-05 21:03:32 +0500

    bug fixx

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -2,8 +2,8 @@ class GymsController < InheritedResources::Base
   before_action :authenticate_user!
 
   def index
-    location_info = request.ip
-    current_user.ip = location_info
+    @location_info = request.ip
+    @current_user.ip = location_info
     puts location_info
     @uloc = request.location
   end
