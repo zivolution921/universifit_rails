6fb28274dae63d63d6e7516f5d8f57f8e64ee368
alizain <alizain721@gmail.com>
2017-11-05 19:48:40 +0500

    Zoonm fixed

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -2,6 +2,10 @@ class GymsController < InheritedResources::Base
   before_action :authenticate_user!
 
   def index
+    @data = Array.new()
+    @userLocation = request.location
+    #@searchResults = Geocoder.search("gym")
+   # @gyms = @searchResults.near(@userLocation, 5000, :order => :distance)
   end
 
 end
