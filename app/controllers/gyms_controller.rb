b389f8f6a3cab6054aea18001e436a81af369d8a
alizain <alizain721@gmail.com>
2017-11-07 10:13:32 +0500

    test

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -2,6 +2,9 @@ class GymsController < InheritedResources::Base
   before_action :authenticate_user!
 
   def index
+    @cordinates = Geocoder.coordinates(request.ip)
+    puts @coordinates[0]
+    puts @cordinates[1]
     @client = GooglePlaces::Client.new("AIzaSyCEdd9xVqSuVr_LOiq_plb0QZe9laI4jV8")
     puts request.location.latitude
     @restaurants = @client.spots(request.location.latitude, request.location.longitude, types: 'gym')
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  def ip
+    "216.52.207.74"
+    end
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
