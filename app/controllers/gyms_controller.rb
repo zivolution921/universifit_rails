e078d0aeb6d2a286888beccddb396719e92a2830
alizain <alizain721@gmail.com>
2017-11-10 11:21:49 +0500

    Gym finalised

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -4,11 +4,10 @@ class GymsController < InheritedResources::Base
   def index
     #@cordinates = Geocoder.coordinates(request.ip)
     @coordinates = Geocoder.coordinates(request.remote_ip)
-    puts "hellldfjldsajflsdajf;jdaslf;safjasd;kjfasjf;safjk;adsjf;dsajfkl;sdakjfdas;o"
     puts request.remote_ip
     puts @coordinates[0]
     @client = GooglePlaces::Client.new("AIzaSyCEdd9xVqSuVr_LOiq_plb0QZe9laI4jV8")
-    @gyms = @client.spots(@coordinates[0], @coordinates[1], types: 'gym',:radius (3000) )
+    @gyms = @client.spots(@coordinates[0], @coordinates[1], types: 'gym', radius: 3000 )
   end
 
 end
