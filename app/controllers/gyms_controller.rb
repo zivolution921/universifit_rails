b2b17cf6237c52b6e366ee26b3499c6c09fc5060
alizain <alizain721@gmail.com>
2017-11-11 08:55:40 +0500

    test

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -8,7 +8,7 @@ class GymsController < InheritedResources::Base
     puts request.remote_ip
     puts @coordinates[0]
     @client = GooglePlaces::Client.new("AIzaSyCEdd9xVqSuVr_LOiq_plb0QZe9laI4jV8")
-    @gyms = @client.spots(@coordinates[0], @coordinates[1], types: 'gym', radius: 3000 )
+    gyms = @client.spots(@coordinates[0], @coordinates[1], types: 'gym', radius: 3000 )
     create_all_gyms(gyms,@coordinates[0],@coordinates[1]) 
     @lobs = Gym.all
   end
