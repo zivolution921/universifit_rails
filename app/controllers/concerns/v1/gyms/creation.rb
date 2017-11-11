933bc14ff56cf0eaae3e2025043f7f20e29e68bb
alizain <alizain721@gmail.com>
2017-11-11 08:10:01 +0500

    test

--- app/controllers/concerns/v1/gyms/creation.rb
+++ app/controllers/concerns/v1/gyms/creation.rb
@@ -3,7 +3,7 @@ module V1
     module Creation 
       extend ActiveSupport::Concern
       def create_all_gyms(gyms)
-        if !gyms.nil?
+        if gyms.nil?
           gyms.each do |g|
             Gym.create(name:g.name, latitude: g.latitude, longitude:g.longitude)
           end
