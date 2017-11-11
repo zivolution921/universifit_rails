d64df21028ef35b1ba2ad8db87f4c8c508665c00
alizain <alizain721@gmail.com>
2017-11-11 09:55:23 +0500

    test

---
+++ app/controllers/concerns/gyms/creation.rb
@@ -0,0 +1,12 @@
+module Gyms
+  module Creation 
+    extend ActiveSupport::Concern
+    def create_all_gyms(gyms,lat,long)
+      if gyms.nil?
+        gyms.each do |g|
+          Gym.create(name:g.name, latitude: lat, longitude:long)
+        end
+      end
+    end
+  end
+end
